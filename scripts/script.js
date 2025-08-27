b6217a25eacf23fa8577e6cbc638ebf5ba575c23
José Carlos Gaspareto Nascimento <josecontato89@gmail.com>
2025-08-27 20:32:18 -0300

    js

--- scripts/script.js
+++ scripts/script.js
@@ -6,3 +6,8 @@ console.log(document.body.childNodes[1]);
 const ListaItens = document.getElementsByTagName('li')
 console.log(ListaItens);
 
+//para pegar o elemento btn e usar evento de escuta click com um pop up (alert)
+const botao = document.getElementById('btn');
+botao.addEventListener('click', function() {
+    alert("Você clicou no botão!")
+});
